2f7264768f7d528828051c177c7106416381479b
noahspott <noahspottdev@gmail.com>
2025-06-30 09:37:32 -0300

    feat: import alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,24 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://lunarlifts.net",
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
   },
 
   integrations: [react(), sitemap()],
