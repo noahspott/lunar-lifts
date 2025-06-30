6be24c5a45bdabc1cf6c41f6ec5c45f6f668a5d8
noahspott <noahspottdev@gmail.com>
2025-06-30 09:38:15 -0300

    feat: page skeletons, home page component skeletons

---
+++ src/components/shared/ContactSection.tsx
@@ -0,0 +1,8 @@
+export default function ContactSection() {
+  return (
+    <section>
+      <h2 className="kicker">Contact Us</h2>
+      <p className="heading-section">Let's Bounce</p>
+    </section>
+  );
+}
---
+++ src/components/shared/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="bg-gradient-dark font-2 text-white">
+      <nav>
+        <h1>Lunar Lifts</h1>
+      </nav>
+    </header>
+  );
+}
