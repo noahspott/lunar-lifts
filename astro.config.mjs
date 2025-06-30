36fbeb5ff6e447ce8daab32e955917682f554e7c
noahspott <noahspottdev@gmail.com>
2025-06-30 08:22:12 -0300

    init: add dependencies, delete unnecessary files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://lunarlifts.net",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react(), sitemap()],
+});
