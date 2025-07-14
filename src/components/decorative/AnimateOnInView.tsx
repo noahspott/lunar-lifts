1c49387e232399b2cbfd5e82a642ae9cc4f7d077
noahspott <noahspottdev@gmail.com>
2025-07-14 10:14:37 -0300

    feat: animate in landing page components

---
+++ src/components/decorative/AnimateOnInView.tsx
@@ -0,0 +1,24 @@
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
+
+export default function AnimateOnInView({
+  children,
+  delay = 0,
+}: {
+  children: React.ReactNode;
+  delay?: number;
+}) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, margin: "-150px" });
+
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0, y: 30 }}
+      animate={isInView ? { opacity: 1, y: 0 } : {}}
+      transition={{ duration: 0.4, delay }}
+    >
+      {children}
+    </motion.div>
+  );
+}
